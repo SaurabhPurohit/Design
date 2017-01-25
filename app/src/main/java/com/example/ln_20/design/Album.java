4329712ca7b7a870580254906c414aef6b60ecbe
Saurabh Purohit <saurabh.letsnurture@gmail.com>
2017-01-25 19:11:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ln_20/design/Album.java
@@ -0,0 +1,44 @@
+package com.example.ln_20.design;
+
+/**
+ * Created by ln-20 on 25/1/17.
+ */
+
+public class Album {
+    private String name;
+    private int numOfSong,thumbnail;
+
+    public Album(){
+    }
+
+    public Album(String name,int numOfSong,int thumbnail){
+        this.name=name;
+        this.numOfSong=numOfSong;
+        this.thumbnail=thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSong() {
+        return numOfSong;
+    }
+
+    public void setNumOfSong(int numOfSong) {
+        this.numOfSong = numOfSong;
+    }
+
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
